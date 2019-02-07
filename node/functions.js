e40ead1b52ceabc4a42cb5d913ce79e729b2f399
DawitAyele2018 <dayele@mail.sfsu.edu>
2019-02-06 20:28:17 -0800

    fuction

--- node/functions.js
+++ node/functions.js
@@ -1,12 +1,12 @@
 // Declaring with the function keyword
-function hi() {
+function hi() { // Try to avoid function
   console.log('Say Hello');
 }
 
 hi();
 
 // arrow functions
-const arrow = (test) => {
+const arrow = (test) => {  // we use this for the class 
   console.log(test);
 }
 
@@ -15,5 +15,13 @@ arrow('hi')
 // for practice
 
 // make an arrow function with no block body
+const myArrow =  (y) => y +1 ;//
+console.log(myArrow(8));
 
 // make a function that returns a function
+
+const functionFactory = () => () => console.log('This is wierd');
+const a = functionFactory();
+a();
+const anotherfunction = functionFactory;
+anotherfunction()();
