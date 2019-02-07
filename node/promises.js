572786c8ccc03aed8dd663b0442e8a1d3e3b51f3
DawitAyele2018 <dayele@mail.sfsu.edu>
2019-02-06 21:07:33 -0800

    promise

--- node/promises.js
+++ node/promises.js
@@ -1,10 +1,11 @@
 const promiseExample = () => {
   return new Promise((resolve) => {
-    setTimeout(() => { resolve('Hello') }, 3000);
+   
   });
 };
 
 promiseExample()
-  .then((res) => console.log(res));
-
+  .then((res)=> res + 'World')
+  .then((res) => console.log(res))
+  .catch((e)=>console.log('I am an error;('));
 console.log('hi');
